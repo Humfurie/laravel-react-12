71830bf4b03dee569362e4c700f025e1e672fafa
Humfurie <Humfurie@gmail.com>
2025-09-28 00:57:07 +0800

    HPL-11 Make a dynamic Policy for blogs

--- app/Policies/BlogPolicy.php
+++ app/Policies/BlogPolicy.php
@@ -4,63 +4,14 @@ namespace App\Policies;
 
 use App\Models\Blog;
 use App\Models\User;
+use App\Traits\HasDynamicPermissions;
 use Illuminate\Auth\Access\Response;
 
 class BlogPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        return false;
-    }
+    use HasDynamicPermissions;
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Blog $blog): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Blog $blog): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Blog $blog): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Blog $blog): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Blog $blog): bool
-    {
-        return false;
-    }
+    // All permission methods are now inherited from the trait
+    // The trait will automatically use 'blog' as the resource name
+    // based on the class name 'BlogPolicy'
 }
