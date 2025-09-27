22c073d3d014aa2c51bbac427a8ece9bf9074fd1
Humfurie <Humfurie@gmail.com>
2025-09-27 15:36:31 +0800

    HPL-7: updating linter errors, and test errors

--- tests/Feature/BlogTest.php
+++ tests/Feature/BlogTest.php
@@ -128,7 +128,7 @@ test('authenticated user can restore deleted blog post', function () {
 });
 
 test('authenticated user can upload blog image', function () {
-    $file = UploadedFile::fake()->image('test-image.jpg');
+    $file = UploadedFile::fake()->image('test-image.jpg', 100, 100)->mimeType('image/jpeg');
 
     $response = $this->actingAs($this->user)
         ->post(route('blogs.upload-image'), [
