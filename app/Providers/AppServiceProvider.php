bf45499f5a53472d80ee3fe2199d08dce6a4be41
Humfurie <Humfurie@gmail.com>
2025-09-28 01:10:10 +0800

    HPL-11 Make a dynamic Policy for blogs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS URLs when behind proxy
+        if (env('FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
+
+        // Set the correct root URL
+        if (env('APP_URL')) {
+            URL::forceRootUrl(env('APP_URL'));
+        }
     }
 }
