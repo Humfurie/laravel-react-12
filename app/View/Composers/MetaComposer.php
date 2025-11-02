b3e29d4b8498482d161ec85808670beea1af75c8
Humphrey Singculan <humfurie@gmail.com>
2025-11-02 01:11:01 +0000

    debug: add logging and improved meta composer logic
    - Add debug comment to check what variables are set
    - Add logging to MetaComposer to track data flow
    - Check component name to ensure giveaway pages are detected
    - Improved conditional logic for meta tag population

--- app/View/Composers/MetaComposer.php
+++ app/View/Composers/MetaComposer.php
@@ -2,20 +2,47 @@
 
 namespace App\View\Composers;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
 
 class MetaComposer
 {
     public function compose(View $view): void
     {
-        $page = $view->getData()['page'] ?? [];
-        
-        $metaTitle = $page['props']['giveaway']['title'] ?? config('app.name');
-        $metaDescription = isset($page['props']['giveaway']['description']) 
-            ? substr($page['props']['giveaway']['description'], 0, 160) 
-            : 'Professional portfolio and blog';
-        $metaImage = $page['props']['giveaway']['primary_image_url'] ?? asset('images/og-default.jpg');
-        
+        $data = $view->getData();
+        $page = $data['page'] ?? [];
+        $component = $page['component'] ?? 'unknown';
+
+        // Debug: Log what we're receiving
+        Log::info('MetaComposer debug', [
+            'component' => $component,
+            'has_page' => isset($data['page']),
+            'has_props' => isset($page['props']),
+            'has_giveaway' => isset($page['props']['giveaway']),
+        ]);
+
+        // Check if this is a giveaway page
+        if (str_contains($component, 'giveaways/show')) {
+            $giveaway = $page['props']['giveaway'] ?? null;
+
+            if ($giveaway) {
+                $metaTitle = $giveaway['title'] ?? config('app.name');
+                $metaDescription = isset($giveaway['description'])
+                    ? substr($giveaway['description'], 0, 160)
+                    : 'Professional portfolio and blog';
+                $metaImage = $giveaway['primary_image_url'] ?? asset('images/og-default.jpg');
+            } else {
+                $metaTitle = config('app.name');
+                $metaDescription = 'Professional portfolio and blog';
+                $metaImage = asset('images/og-default.jpg');
+            }
+        } else {
+            // Default meta tags for non-giveaway pages
+            $metaTitle = config('app.name');
+            $metaDescription = 'Professional portfolio and blog';
+            $metaImage = asset('images/og-default.jpg');
+        }
+
         $view->with(compact('metaTitle', 'metaDescription', 'metaImage'));
     }
 }
